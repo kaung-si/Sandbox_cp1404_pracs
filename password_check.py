129c8d47a4088440569d26cc2891331c384eb734
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-04 20:30:57 +0630

    Add function that check if the input follow the rule

--- password_check.py
+++ password_check.py
@@ -6,13 +6,20 @@ Name: Kaung Sithu
 
 
 def main():
-    """Print the number of character input as asterisks."""
+    """Start Program"""
     text = input("Enter a string: ")
     print(count_character(text))
+    print("Does it comply with the rule? {}".format(check_char(text)))
 
 
 def count_character(txt):
+    """Return the length of string as asterisks."""
     return len(txt) * "*"
+
+
+def check_char(txt):
+    """Check whether the input text comply the rule."""
+    return 8 <= len(txt) <= 20 and '#' in txt
 
 
 main()
