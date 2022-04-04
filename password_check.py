2c0e97e1bdeac673bd800684650748d64db4b3d7
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-04 19:32:46 +0630

    Add docstring to password check program

--- password_check.py
+++ password_check.py
@@ -1,2 +1,18 @@
-text = input("Enter a string: ")
-print(len(text) * "*")
+"""
+Description: A simple Password Character Counter
+
+Name: Kaung Sithu
+"""
+
+
+def main():
+    """Print the number of character input as asterisks."""
+    text = input("Enter a string: ")
+    print(count_character(text))
+
+
+def count_character(txt):
+    return len(txt) * "*"
+
+
+main()
